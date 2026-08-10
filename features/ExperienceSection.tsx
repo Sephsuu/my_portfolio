596611c60d8afb20fd2ef05979d61c4f86548fae
Sephsuu <batallerjem208@gmail.com>
2026-08-10 20:30:13 +0800

    ignore case

--- features/ExperienceSection.tsx
+++ features/ExperienceSection.tsx
@@ -169,7 +169,7 @@ export function ExperienceSection() {
                                                 className="h-auto rounded-full bg-roast px-5 py-2.5 text-sm text-white"
                                                 disabled
                                             >
-                                                Not availabler
+                                                Not available
                                             </Button>
                                         </HoverCardTrigger>
                                         <HoverCardContent>
